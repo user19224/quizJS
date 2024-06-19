83ba3fdfdf2316b3ab0a4374fbf1ba7fa9b33bf7
Gleb Moskovkin <mgleb914@gmail.com>
2024-06-19 23:52:22 +0300

    updated styles

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Массив вопросов
-    const questions = [
+
+const questions = [
         {
             question: "Назовите столицу Франции?",
             answers: ["Нью-Йорк", "Лондон", "Париж", "Минск"],
@@ -21,9 +20,54 @@ document.addEventListener("DOMContentLoaded", function() {
             answers: ["Барселона", "Мадрид", "Лиссабон", "Париж"],
             correct: 1
         }
-    ];
-    
-    
-    
-    
-});
+];
+  
+
+
+let questionIndex = 0;
+let score = 0;
+
+const questionNumberElement = document.getElementById('question-number')
+const questionTextElement = document.getElementById("question-text")
+const answerButtonElement = document.getElementById("answer-buttons")
+const nextButton = document.getElementById('next-button')
+const backButton = document.getElementById('back-button')
+const resultContainer = document.getElementById('result-container')
+const scoreElement = document.getElementById('score')
+const restartButton = document.getElementById('restart-button')
+
+
+
+function showQuestions(){
+        
+        
+        questionNumberElement.textContent = `Вопрос № ${questionIndex + 1}`
+        questionTextElement.textContent = questions.question
+        
+        answerButtonElement.innerHTML = ""; 
+
+        questions.answers.forEach(() => {
+            const button  = document.createElement('button')
+            button.classList.add('btn')
+            button.textContent = 
+
+        });
+        
+
+
+    nextButton.disabled = true
+}
+
+
+function selectAnswer(){
+
+}
+
+
+
+function showResult(){
+   
+}
+
+nextButton.addEventListener('click',showQuestions)
+
